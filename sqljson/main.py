1b65bfc26a7bc2800c9e30ffd2886930a7d1d23d
Maxime <maxime.paille@mcn.gouv.qc.ca>
2023-10-03 21:25:31 -0400

    add handle multiple json as input, add fix search in nested

--- sqljson/main.py
+++ sqljson/main.py
@@ -6,6 +6,33 @@ import sys
 import argparse
 
 
+def process_conditions(condition_part, df):
+    conditions = re.split(r'\s+(and|or)\s+', condition_part)
+    adjusted_conditions = []
+
+    for i in range(0, len(conditions), 2):
+        condition = adjust_condition_to_dtype(conditions[i].strip(), df)
+        for column in df.columns:
+            if "." in column:
+                condition = condition.replace(column, f"`{column}`")
+        adjusted_conditions.append(condition)
+
+        if i + 1 < len(conditions):
+            adjusted_conditions.append(conditions[i + 1])
+
+    return " ".join(adjusted_conditions)
+def process_input():
+    """Yield each JSON object from stdin."""
+    buffer = ""
+    for line in sys.stdin:
+        buffer += line
+        try:
+            yield json.loads(buffer)
+            buffer = ""
+        except json.JSONDecodeError:
+            continue
+
+
 def adjust_condition_to_dtype(condition, df):
     parts = condition.split()
     if parts[0] in df.columns:
@@ -21,6 +48,11 @@ def process_conditions(condition_part, df):
 
     for i in range(0, len(conditions), 2):
         condition = adjust_condition_to_dtype(conditions[i].strip(), df)
+
+        for column in df.columns:
+            if "." in column:
+                condition = condition.replace(column, f"`{column}`")
+
         adjusted_conditions.append(condition)
 
         if i + 1 < len(conditions):
@@ -44,7 +76,8 @@ def flatten_data(data):
         output.append(data)
     return output
 
-def run_query(json_data, query):
+
+def run_query(json_data, query, debug=False):
     try:
         df = pd.json_normalize(json_data)
 
@@ -52,6 +85,10 @@ def run_query(json_data, query):
             select_cols = df.columns.tolist()
         else:
             select_cols = [col.strip() for col in query.split("from")[0].replace("select", "").strip().split(",")]
+
+        for col in select_cols:
+            if col not in df.columns:
+                raise KeyError(f"Column '{col}' not found in data.")
 
         if not query.lower().startswith("select"):
             raise ValueError("Only SELECT queries are supported.")
@@ -64,36 +101,40 @@ def run_query(json_data, query):
         else:
             result = df[select_cols]
 
-        # Flatten the resulting data
         flattened_results = [flatten_data(row) for row in result.values.tolist()]
         return flattened_results
 
-    except pd.errors.UndefinedVariableError as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-    except Exception as e:
-        print(f"Unexpected Error: {e}")
-        sys.exit(1)
-
+    except (pd.errors.UndefinedVariableError, KeyError, Exception) as e:
+        if debug:
+            if isinstance(e, KeyError):
+                print(f"Error: {e}")
+            elif isinstance(e, pd.errors.UndefinedVariableError):
+                print(f"Error: {e}")
+                sys.exit(1)
+            else:
+                print(f"Unexpected Error: {e}")
+                sys.exit(1)
+        return []
 
 def main():
     parser = argparse.ArgumentParser(description='Run SQL-like queries against JSON data.')
     parser.add_argument('-q', '--query', help='SQL-like query Ex: select ')
     parser.add_argument('-s', '--separator', default=",", help='Output format separator')
     parser.add_argument('-d', '--describe', action='store_true', help='Display all column names')
+    parser.add_argument('-v', '--debug', action='store_true', help='Enable detailed error messages')
+
     args = parser.parse_args()
 
+    for json_data in process_input():
+        if args.describe:
+            df = pd.json_normalize(json_data)
+            print("\n".join(df.columns))
+            continue
 
-    json_data = json.load(sys.stdin)
-    if args.describe:
-        df = pd.json_normalize(json_data)
-        print("\n".join(df.columns))
-        return
-
-    if args.query:
-        results = run_query(json_data, args.query)
-        for row in results:
-            print(args.separator.join(map(str, row)))
+        if args.query:
+            results = run_query(json_data, args.query, debug=args.debug)
+            for row in results:
+                print(args.separator.join(map(str, row)))
 
 
 if __name__ == "__main__":
